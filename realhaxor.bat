57b8ea2fd656ec72ab68c526c173beac9c438f01
angelsflyinhell <azami@namespace.media>
2021-03-02 09:05:21 +0100

    Create realhaxor.bat

---
+++ realhaxor.bat
@@ -0,0 +1,15 @@
+color a 
+cd ../../
+@echo off
+cls
+
+:1
+set /a num=%RANDOM% %%10
+
+echo %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num% %num%
+
+tree 
+
+goto 1
+
+pause
